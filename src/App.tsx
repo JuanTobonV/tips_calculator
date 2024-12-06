e22797172a8f0cafa71998a00d29d818a5db7eab
Juan Villa <jujotovil@gmail.com>
2024-12-05 22:16:58 -0500

    starting main component, adding db

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { menuItems } from "./data/data"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  console.log(menuItems);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body className="bg-gray-100 h-screen">
+
+        <header className="py-5">
+          <h1 className="text-center text-3xl font-semibold">Calculadora de propinas y consumos</h1>
+        </header>
+  
+        <main className="max-w-7xl mx-auto max-h-fit grid grid-cols-2 bg-cyan-100 rounded-xl">
+            <div className="p-8">
+              <h2 className="text-center font-semibold text-xl">Menu</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci dolores quas delectus iste impedit fugit repudiandae, minus provident, cupiditate ut assumenda libero quia. Possimus nulla repudiandae, sed odit impedit tenetur perspiciatis dicta sequi rem cumque expedita consequuntur nemo error maxime nesciunt libero sit ex reiciendis modi magnam minima. Similique adipisci unde dolore. Nesciunt porro suscipit harum omnis? Accusamus exercitationem praesentium voluptates dicta aspernatur. Labore iure voluptate nostrum sint corrupti error molestiae accusantium recusandae? Fugiat, dolor quae enim recusandae officiis animi consequatur consectetur quos voluptatum deserunt, et qui hic excepturi atque id? Sit incidunt eius blanditiis aut quis fuga nemo! Ipsam!</p>
+  
+            </div>
+  
+            <div className="bg-white max-h-full rounded-r-xl p-8">
+              <h2 className="text-center font-semibold text-xl">Bill</h2>
+  
+            </div>
+        </main>
+      </body>
     </>
   )
 }
