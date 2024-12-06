37f1652bc21d2905fa2f1ba82c700563ba65a0b8
Juan Villa <jujotovil@gmail.com>
2024-12-05 23:43:47 -0500

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,19 @@ function App() {
   console.log(menuItems);
   return (
     <>
-      <body className="bg-gray-100 h-screen">
+      <body className="bg-gray-100 h-screen flex">
 
-        <header className="py-5">
-          <h1 className="text-center text-3xl font-semibold">Calculadora de propinas y consumos</h1>
-        </header>
+        
   
-        <main className="max-w-7xl mx-auto max-h-fit grid grid-cols-2 bg-cyan-100 rounded-xl">
-            <div className="p-8">
-              <h2 className="text-center font-semibold text-xl">Menu</h2>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci dolores quas delectus iste impedit fugit repudiandae, minus provident, cupiditate ut assumenda libero quia. Possimus nulla repudiandae, sed odit impedit tenetur perspiciatis dicta sequi rem cumque expedita consequuntur nemo error maxime nesciunt libero sit ex reiciendis modi magnam minima. Similique adipisci unde dolore. Nesciunt porro suscipit harum omnis? Accusamus exercitationem praesentium voluptates dicta aspernatur. Labore iure voluptate nostrum sint corrupti error molestiae accusantium recusandae? Fugiat, dolor quae enim recusandae officiis animi consequatur consectetur quos voluptatum deserunt, et qui hic excepturi atque id? Sit incidunt eius blanditiis aut quis fuga nemo! Ipsam!</p>
+        <main className="  max-w-7xl mx-auto my-auto max-h-fit grid grid-cols-2 bg-cyan-100 rounded-xl">
+            <div className="bg-white p-8 rounded-l-xl">
+              <h2 className="pb-4 font-bold text-2xl">Menu</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque suscipit labore quas voluptas molestiae similique, reiciendis autem alias, ut inventore culpa nihil eius architecto earum expedita, asperiores porro quam. Dignissimos eligendi doloribus quia harum autem consectetur, modi odio culpa. Perspiciatis consequuntur atque totam beatae eligendi, incidunt alias accusamus corrupti nobis, expedita qui reprehenderit. Accusantium, veniam quae? Eveniet odit incidunt rem minus ex error earum et obcaecati cupiditate dolor cum ab a temporibus doloribus modi repellendus tempore, illum assumenda facilis saepe rerum reiciendis officiis ipsa nulla. Inventore aut cum itaque facere vitae officiis esse laboriosam placeat magni temporibus consequuntur, ipsum quas? Enim quibusdam, error distinctio sunt unde commodi rerum reprehenderit aut similique ipsum qui neque atque, voluptates ipsam sequi inventore id veritatis, assumenda dolorem quidem. Cumque consequuntur architecto quibusdam inventore at, atque provident quaerat consectetur, reiciendis sint corrupti voluptatum. Voluptatem consequuntur suscipit optio, dolore pariatur repellendus voluptate quisquam esse tempore nam quam cupiditate quod sapiente. Pariatur tenetur provident incidunt distinctio harum animi aliquid expedita dolores, deleniti voluptatum, ad numquam vero, totam ducimus nulla culpa. Numquam illum porro eaque earum, eius et delectus nam pariatur error magni ducimus velit dolore consequuntur nisi nihil autem ratione quas aut, sapiente, doloremque perferendis. Incidunt, pariatur.</p>
   
             </div>
   
-            <div className="bg-white max-h-full rounded-r-xl p-8">
-              <h2 className="text-center font-semibold text-xl">Bill</h2>
-  
+            <div className="max-h-full rounded-r-xl p-8 shadow-[-3px_4px_6px_0px_rgba(0,_0,_0,_0.1)]">
+              <h2 className="pb-4 font-bold text-2xl">Bill</h2>
             </div>
         </main>
       </body>
